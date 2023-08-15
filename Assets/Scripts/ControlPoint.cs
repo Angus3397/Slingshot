c45b4a59c5cdeeb78486658241b7b56baef9baa2
Angus <angusong26@gmail.com>
2023-08-15 18:03:42 +0800

    ending for today, tired

--- Assets/Scripts/ControlPoint.cs
+++ Assets/Scripts/ControlPoint.cs
@@ -10,16 +10,43 @@ public class ControlPoint : MonoBehaviour
 
     public float rotateSpeed = 5f;
 
+    public float shotPower = 5f;
+
+    public LineRenderer aimLine;
+
     // Update is called once per frame
     void Update()
     {
         transform.position = playerBall.position;
 
+        // Controls the player camera
         if (Input.GetMouseButton(0)) 
         {
             xRotate += Input.GetAxis("Mouse X") * rotateSpeed;
             yRotate += Input.GetAxis("Mouse Y") * rotateSpeed;
-            transform.rotation = Quaternion.Euler(-yRotate, xRotate, 0f);
+            if (yRotate < -35f) 
+            {
+                yRotate = -35f;
+            }
+            /*if (yRotate > 11f)
+            {
+                yRotate = 11f;
+            }
+            else if (yRotate < -170f) 
+            {
+                yRotate = -170f;
+            }*/
+            transform.rotation = Quaternion.Euler(yRotate, xRotate, 0f);
+            aimLine.gameObject.SetActive(true);
+            aimLine.SetPosition(0, transform.position);
+            aimLine.SetPosition(1, transform.position + transform.forward * 7f);
+        }
+
+        // Shooting function
+        if (Input.GetMouseButtonUp(0)) 
+        {
+            playerBall.velocity = transform.forward * shotPower;
+            aimLine.gameObject.SetActive(false);
         }
     }
 }
